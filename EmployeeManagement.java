c40984970b1262c3ad418672403b6cd63ef9bbbf
arnav338 <arnavmalhotra338@gmail.com>
2020-08-05 15:08:47 +0530

    div module added

--- EmployeeManagement.java
+++ EmployeeManagement.java
@@ -5,4 +5,7 @@ public void add(int a,int b){
 public void diff(int a,int b){
    System.out.println("Difference :"+(a-b));
 }
+public void div(int a,int b){
+   System.out.println("Quotient :"+(a/b));
+}   
 }
